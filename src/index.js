4da36278d56d5c1cf4fd9fd3fce4cb46ae08dc73
Erik Z <molagg92@gmail.com>
2023-04-27 13:00:48 -0700

    got my files to display in dev tools the way they should be, ready to start jest testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 import './css/styles.css';
+import HomoSapien from "./service.js";
+
+window.addEventListener("load", function () {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function (e) { galaxyCalculator(e); });
+});
+function galaxyCalculator(e) {
+  e.preventDefault();
+  const input = document.getElementById('inputBtn').value;
+  const erik = new HomoSapien (31);
+  console.log(erik);
+  console.log (input);
+}
+
---
+++ src/service.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+export default class HomoSapien {
+  constructor(age) {
+    this.age = age;
+  } superGalaxyCalc() {
+    const mercuryAge = Math.round(this.age / 0.24);
+    return mercuryAge;
+  }
+}
+
+
+//  const input = document.getElementById('inputBtn');
+// const inputBday = parseInt(input);
