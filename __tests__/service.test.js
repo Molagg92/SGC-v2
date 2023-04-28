4d4a837d753ee4f2307591511f9f92f88929b30e
Erik Z <molagg92@gmail.com>
2023-04-28 11:11:11 -0700

    make my calculator work for earth and mercury years

--- __tests__/service.test.js
+++ __tests__/service.test.js
@@ -4,8 +4,12 @@ describe('HomoSapien', () => {
   let erik;
 beforeEach(() => {
     erik = new HomoSapien(31);
-});
+  });
 test('should make a new HomoSapien object, return its earth years', () => {
 expect(erik.earth).toEqual(31);
-});
+  });
+test('should retrun Onjects earth age in mercury years. 129', () => {
+expect(erik.earth).toEqual(31);
+expect(erik.mercury).toEqual(129);
+  });
 });
--- src/service.js
+++ src/service.js
@@ -2,5 +2,8 @@
 export default class HomoSapien {
   constructor(age) {
     this.earth = age;
+    let mercuryAge = Math.round(age / .24);
+
+    this.mercury = mercuryAge;
   }
 }
