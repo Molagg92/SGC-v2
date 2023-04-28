b3475f7a37367045d33e1a2f7e7ee0056d68f43a
Erik Z <molagg92@gmail.com>
2023-04-28 13:33:12 -0700

    also add both index files to the commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,47 @@
 import './css/styles.css';
 import HomoSapien from "./service.js";
 
-window.addEventListener("load", function () {
-  const form = document.querySelector("form");
-
-  form.addEventListener("submit", function (e) { galaxyCalculator(e); });
-});
 function galaxyCalculator(e) {
   e.preventDefault();
-  const input = document.getElementById('inputBtn').value;
-  
-  console.log(erik.mercuryCalc());
-  console.log (input);
+  const userAge = parseInt(document.querySelector("#userAge").value);
+  const userAgeReturn = new HomoSapien(userAge);
+  document.querySelector("#response").innerText = null;
+  const merc = document.createElement("p");
+  merc.append(userAgeReturn.mercury);
+  const ven = document.createElement("p");
+  ven.append(userAgeReturn.venus);
+  const mars = document.createElement("p");
+  mars.append(userAgeReturn.mars);
+  const jup = document.createElement("p");
+  jup.append(userAgeReturn.jupiter);
+  document.querySelector("#response").append(merc);
+  document.querySelector("#response").append("Venus age:");
+  document.querySelector("#response").append(ven);
+  document.querySelector("#response").append("Mars age:");
+  document.querySelector("#response").append(mars);
+  document.querySelector("#response").append("Jupiter age:");
+  document.querySelector("#response").append(jup);
 }
-const erik = new HomoSapien (31);
+
+function PastCalc(e) {
+  e.preventDefault();
+  const currentAge = parseInt(document.querySelector("#currentAge").value);
+  const pastYears = new HomoSapien(currentAge);
+  const pastAge = parseInt(document.querySelector("#pastAge").value);
+  document.querySelector("#response2").innerText = null;
+  document.querySelector("#response2").append(pastYears.yearsPassed(pastAge));
+}
+function FutureCalc(e) {
+  e.preventDefault();
+  const currentAge2 = parseInt(document.querySelector("#currentAge2").value);
+  const futureYears = new HomoSapien(currentAge2);
+  const futureAge = parseInt(document.querySelector("#futureAge").value);
+  document.querySelector("#response3").innerText = null;
+  document.querySelector("#response3").append(futureYears.yearsToGo(futureAge));
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("#UserForm").addEventListener("submit", galaxyCalculator);
+  this.document.querySelector("#pastForm").addEventListener("submit", PastCalc);
+  this.document.querySelector("#futureForm").addEventListener("submit", FutureCalc);
+});
