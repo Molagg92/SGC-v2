5295b81fb3d84fedcfbf652f8744719011df26bd
Erik Z <molagg92@gmail.com>
2023-04-28 11:19:46 -0700

    add more plantest to the line up, the math so far works out

--- __tests__/service.test.js
+++ __tests__/service.test.js
@@ -12,4 +12,11 @@ test('should retrun Onjects earth age in mercury years. 129', () => {
 expect(erik.earth).toEqual(31);
 expect(erik.mercury).toEqual(129);
   });
+test('should return age in Venus, Mars, and Jupiter years', () => {
+expect(erik.earth).toEqual(31);
+expect(erik.mercury).toEqual(129);
+expect(erik.venus).toEqual(50);
+expect(erik.mars).toEqual(16);
+expect(erik.jupiter).toEqual(3);
+  });
 });
--- src/service.js
+++ src/service.js
@@ -3,7 +3,14 @@ export default class HomoSapien {
   constructor(age) {
     this.earth = age;
     let mercuryAge = Math.round(age / .24);
+    let venusAge = Math.round(age / .62);
+    let marsAge = Math.round(age / 1.88);
+    let jupiterAge = Math.round(age / 11.86);
+
 
     this.mercury = mercuryAge;
+    this.venus = venusAge;
+    this.mars = marsAge;
+    this.jupiter = jupiterAge;
   }
 }
