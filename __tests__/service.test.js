75ff3755da591419dbfc593ebe1641892d004e77
Erik Z <molagg92@gmail.com>
2023-04-28 09:57:24 -0700

    rewrote the project a second time, but whiteboarded a new solution

--- __tests__/service.test.js
+++ __tests__/service.test.js
@@ -1,38 +1,11 @@
 import HomoSapien from "../src/service.js";
 
 describe('HomoSapien', () => {
-test('should return erik as 30 and 1992', () => {
-    const erik = new HomoSapien(30);
-expect(erik.age).toEqual(30);
+  let erik;
+beforeEach(() => {
+    erik = new HomoSapien(31);
 });
-});
-
-
-describe('mercuryCalc', () => {
-test('should retrun my current age in mercury years! 129', () => {
-    const erik = new HomoSapien(31);
-expect(erik.mercuryCalc()).toEqual(129);
-});
-});
-
-describe('venusCalc', () => {
-test('should retrun my current age in venus years! 50', () => {
-    const erik = new HomoSapien(31);
-expect(erik.venusCalc()).toEqual(50);
-});
-});
-
-
-describe('marsCalc', () => {
-test('should retrun my current age in mars years! 16', () => {
-    const erik = new HomoSapien(31);
-expect(erik.marsCalc()).toEqual(16);
-});
-});
-
-describe('jupiterCalc', () => {
-test('should retrun my current age in jupiter years! 3', () => {
-    const erik = new HomoSapien(31);
-expect(erik.jupiterCalc()).toEqual(3);
+test('should make a new HomoSapien object, return its earth years', () => {
+expect(erik.earth).toEqual(31);
 });
 });
--- src/service.js
+++ src/service.js
@@ -1,22 +1,6 @@
 /* eslint-disable no-console */
 export default class HomoSapien {
   constructor(age) {
-    this.age = age;
-  } mercuryCalc() {
-    const mercuryAge = Math.round(this.age / 0.24);
-    return mercuryAge;
-  } venusCalc(){
-    const venusAge = Math.round(this.age / .62);
-    return venusAge;
-  } marsCalc(){
-    const marsAge = Math.round(this.age / 1.88);
-    return marsAge;
-  } jupiterCalc() {
-    const jupiterAge = Math.round(this.age / 11.86);
-    return jupiterAge;
+    this.earth = age;
   }
 }
-
-
-//  const input = document.getElementById('inputBtn');
-// const inputBday = parseInt(input);
